1d08470aa39b595692f7183c58e4cb42bd96da20
Shan <sraw0830@740s-pl-101.local>
2018-11-27 12:07:20 -0700

    Created AppFrame and AppPanel; created framework for AppFrame

---
+++ src/gui/view/AppFrame.java
@@ -0,0 +1,27 @@
+package gui.view;
+
+import javax.swing.JFrame;
+
+import gui.controller.GUIController;
+
+public class AppFrame extends JFrame
+{
+	private AppPanel appPanel;
+	
+	public AppFrame(GUIController controller)
+	{
+		super();
+		
+		appPanel = new AppPanel(controller);
+	}
+	
+	private void setupFrame()
+	{
+		setContentPane(appPanel);
+		setSize(800, 800);
+		setTitle("Sample GUI");
+		setResizable(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+}
---
+++ src/gui/view/AppPanel.java
@@ -0,0 +1,10 @@
+package gui.view;
+
+import javax.swing.JPanel;
+
+import gui.controller.GUIController;
+
+public class AppPanel extends JPanel
+{
+	
+}
